f7d2d3b8d2d46ff8b9434016a9d2986bb72d193c
Bernardo Magueta Kowalsky <kwybernardo@gmail.com>
2023-08-09 12:27:32 -0300

    commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,18 +2,11 @@ module.exports = {
   formatter: '@commitlint/format',
   parserPreset: './parser.commit.js',
   rules: {
-    'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
-    'footer-leading-blank': [1, 'always'],
-    'footer-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'always', 100],
     'scope-case': [2, 'always', ['upper-case']],
     'scope-empty': [2, 'always'],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [0, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'upper-case'],
-    'type-empty': [2, 'always'],
     'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
   },
   prompt: {
