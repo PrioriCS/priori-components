a0f9ba85bdddd8a70b9ad9151e1e92de486e3ace
BeKowalsky <BeKowalsky@users.noreply.github.com>
2023-08-20 12:52:00 +0000

    Prettified Code!

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true,
 });
-Object.defineProperty(exports, "Bold", {
+Object.defineProperty(exports, 'Bold', {
   enumerable: true,
   get: function get() {
     return _Bold.default;
   },
 });
-var _Bold = _interopRequireDefault(require("./components/Bold"));
+var _Bold = _interopRequireDefault(require('./components/Bold'));
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
 }
