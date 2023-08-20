b76774fdc183054b83e4c4ff7d535dd79a1f7b21
BeKowalsky <BeKowalsky@users.noreply.github.com>
2023-08-20 12:31:33 +0000

    Prettified Code!

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 Object.defineProperty(exports, "Bold", {
   enumerable: true,
   get: function get() {
     return _Bold.default;
-  }
+  },
 });
 var _Bold = _interopRequireDefault(require("./components/Bold"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
--- src/lib/components/Bold.js
+++ src/lib/components/Bold.js
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export default function Bold(props) {
-  return <span className='font-bold' {...props} />;
+  return <span className="font-bold" {...props} />;
 }
