3a7d0efa369ebeb223524d6c2a54a61304d94dbf
BeKowalsky <kwybernardo@gmail.com>
2023-08-20 10:10:56 -0300

    FEAT: Config jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const JsConfigPathsMapper = require('jsconfig-paths-jest-mapper');
+
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: new JsConfigPathsMapper({ configFileName: './jsconfig.json' }),
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+};
