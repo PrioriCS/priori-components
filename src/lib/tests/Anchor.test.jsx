366f67374fff91bda6a571d4e6bf057e520251bd
BeKowalsky <kwybernardo@gmail.com>
2023-08-21 15:48:00 -0300

    FIX: test actions

--- src/lib/tests/Anchor.test.jsx
+++ src/lib/tests/Anchor.test.jsx
@@ -4,6 +4,7 @@ import { expect, test } from '@jest/globals';
 import { create } from 'react-test-renderer';
 import Anchor from '../components/Anchor';
 
+//Melhorar teste
 describe('Create a AccordionMenu Component', () => {
   test('check anchor children', () => {
     const component = create(<Anchor />);
