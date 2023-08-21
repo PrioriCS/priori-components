5d1a6a2cff4e1f4c8546ffeddc59d748e3545d16
BeKowalsky <kwybernardo@gmail.com>
2023-08-21 10:48:56 -0300

    FEAT: NPM Actions Publish

---
+++ src/lib/tests/Anchor.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { expect, test } from '@jest/globals';
+import { create } from 'react-test-renderer';
+import { Anchor } from 'prioricomponents';
+
+describe('Create a AccordionMenu Component', () => {
+  test('check anchor children', () => {
+    const component = create(<Anchor />);
+    expect(component.toTree().rendered.type).toBe('a');
+  });
+});
