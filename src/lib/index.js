598cd014565c779f902a9579a39127167cb9086f
arturfurtado <artururtado@gmail.com>
2023-08-31 16:20:12 -0300

    FEAT: Status Component

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,5 +3,9 @@ import Anchor from './components/Anchor';
 import HelpItem from './components/HelpItem';
 import Row from './components/Row';
 import ButtonWrapper from './components/ButtonWrapper';
+import Label from './components/Label';
+import Labeled from './components/Labeled';
+import ToggleSwitchButton from './components/ToggleSwitchButton';
+import Status from './components/Status';
 
-export { Bold, Anchor, HelpItem, ButtonWrapper, Row };
+export { Bold, Anchor, HelpItem, ButtonWrapper, Row, Label, Labeled, ToggleSwitchButton, Status };
