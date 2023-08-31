42e988d251327aaa87aac696399e137511b135b7
arturfurtado <artururtado@gmail.com>
2023-08-31 17:13:03 -0300

    FEAT: Select Section

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,5 +3,10 @@ import Anchor from './components/Anchor';
 import HelpItem from './components/HelpItem';
 import Row from './components/Row';
 import ButtonWrapper from './components/ButtonWrapper';
+import Label from './components/Label';
+import Labeled from './components/Labeled';
+import ToggleSwitchButton from './components/ToggleSwitchButton';
+import Status from './components/Status';
+import SelectSection from './components/SelectSection';
 
-export { Bold, Anchor, HelpItem, ButtonWrapper, Row };
+export { Bold, Anchor, HelpItem, ButtonWrapper, Row, Label, Labeled, ToggleSwitchButton, Status, SelectSection };
