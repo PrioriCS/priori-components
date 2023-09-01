81579fc1adbb0e1887a243a47abce49df27e4419
arturfurtado <artururtado@gmail.com>
2023-09-01 15:05:25 -0300

    FEAT: IconButton Component

---
+++ src/lib/components/IconButton.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export default function IconButton({
+  icon: Icon,
+  small,
+  base,
+  className = '',
+  bgColor = 'grayWeak',
+  borderColor = 'primaryStrong',
+  radius = 'full',
+  ...rest
+}) {
+  const availableBackgroundColors = {
+    grayWeak: 'bg-gray-200 text-gray-500',
+    white: 'bg-white text-primary-700 hover:bg-primary-700 hover:text-white transition-all hover:transition-all delay-100',
+  };
+
+  const avaliableBordersColors = {
+    primaryStrong: 'border border-primary-700',
+    none: 'border-none',
+  };
+
+  const availableBordersRadius = {
+    rounded: 'rounded',
+    full: 'rounded-full',
+  };
+
+  return (
+    <button
+      type='button'
+      className={twMerge(availableBackgroundColors[bgColor], avaliableBordersColors[borderColor],  className,availableBordersRadius[radius], 'p-1 h-fit')}
+      {...rest}>
+      <Icon className={`${small ? 'text-sm' : base ? 'text-base' : 'text-lg'}`} data-testid='icon' />
+    </button>
+  );
+}
