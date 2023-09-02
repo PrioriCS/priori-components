b2d388535d108c9d48fef1f4e1bf43c75f78738b
BeKowalsky <kwybernardo@gmail.com>
2023-09-02 17:11:05 -0300

    FIX: Build

--- src/lib/components/ToggleSwitchButton.jsx
+++ src/lib/components/ToggleSwitchButton.jsx
@@ -5,8 +5,10 @@ export default function ToggleSwitchButton({ active, ...rest }) {
   return (
     <div className='w-8 h-4 flex items-center p-1 relative rounded-full' data-testid='toggle-switch' {...rest}>
       <span
-        className={twMerge('absolute cursor-pointer top-0 left-0 right-0 bottom-0 rounded-full transition-all before:absolute before:content=[""] before:h-3 before:w-3 before:left-0.5 before:bottom-0.5 before:bg-white before:rounded-full before:transition-all', 
-          active ? 'before:translate-x-4 bg-sky-500' : 'bg-gray-400')}
+        className={twMerge(
+          'absolute cursor-pointer top-0 left-0 right-0 bottom-0 rounded-full transition-all before:absolute before:content=[""] before:h-3 before:w-3 before:left-0.5 before:bottom-0.5 before:bg-white before:rounded-full before:transition-all',
+          active ? 'before:translate-x-4 bg-sky-500' : 'bg-gray-400'
+        )}
       />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,14 @@
+const colors = require('tailwindcss/colors');
+
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/**/*.js',
@@ -8,7 +19,23 @@ module.exports = {
     './src/lib/components/*.jsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        primary: {
+          ...colors.blue,
+          1000: '#14275c', // 900 but with 1/3 of brightness (value in HSV)
+        },
+        secondary: colors.teal,
+      },
+      fontFamily: {
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        sweep: 'transition 1s ease-in-out',
+      },
+    },
   },
+
   plugins: [],
 };
