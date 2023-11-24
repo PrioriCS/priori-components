59f80255785997c61251a69fc7e572cc8046fc83
BeKowalsky <kwybernardo@gmail.com>
2023-11-24 10:29:25 -0300

    FIX: Test

--- src/lib/tests/Checkbox.test.jsx
+++ src/lib/tests/Checkbox.test.jsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Checkbox from '../components/Input/Checkbox';
 
 describe('Checkbox', () => {
   it('should pass additional props to the checkbox element', () => {
-    const checkbox = render(<Checkbox id="my-checkbox" name="my-checkbox" />);
+    const checkbox = render(<Checkbox id='my-checkbox' name='my-checkbox' />);
 
     expect(checkbox.container.querySelector('input')).toHaveAttribute('id', 'my-checkbox');
     expect(checkbox.container.querySelector('input')).toHaveAttribute('name', 'my-checkbox');
